ec3112f5c2dfdbaf6550b03670879f31ed17bf2c
Noor-Aysha Saadat <naysaadat@gmail.com>
2025-02-14 01:50:39 -0800

    set up basic react app using vite (industry standard)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <p>This is a basic React app created with Vite.</p>
+    </div>
+  )
+}
+
+export default App
