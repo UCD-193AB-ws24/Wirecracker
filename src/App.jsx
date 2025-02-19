f1190680fda75124c454d42ca50f00f3f5a0d821
chr-wang <104956901+chr-wang@users.noreply.github.com>
2025-02-18 19:24:51 -0800

    Add starting login and review tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import "./App.css"
+export default function Home() {}
 
-function App() {
+export function Center() {
   return (
-    <div className="App">
-      <h1>Welcome to My React App</h1>
-      <p>This is a basic React app created with Vite.</p>
+    <div className="flex-col justify-center">
+      <Logo />
+      <Login />
     </div>
   )
 }
 
-export default App
+export function Left() {
+  return (
+    <div>
+      <h2>My Stuff</h2>
+      <ToReview />
+    </div>
+  )
+}
+
+function Logo() {
+  return (
+    <div>
+      <img alt="Logo"/>
+      <h1>Wirecracker</h1>
+    </div>
+  )
+}
+
+function Login() {
+  return (
+    <div>
+      {/* Enable and change onClick when login added */}
+      <button
+        className="bg-slate-300 px-5 py-3 rounded"
+        disabled
+        onClick={() => alert("Feature not available.") }
+      >
+        Login
+      </button>
+    </div>
+  )
+}
+
+function ToReview() {
+  return (
+    <div className="text-violet-500 flex gap-x-2">
+      {/* Triangle */}
+      <div class="before:content-['▸']"></div>
+      <p>To Review</p>
+    </div>
+  )
+}
