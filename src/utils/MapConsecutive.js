8d20895509a98851c17a4ed84c82513008dc52a2
chr-wang <104956901+chr-wang@users.noreply.github.com>
2025-05-07 11:23:25 -0700

    fix: list all possible contact pairs

---
+++ src/utils/MapConsecutive.js
@@ -0,0 +1,12 @@
+/**
+ * Code from https://labex.io/tutorials/javascript-map-consecutive-elements-28479
+ * 
+ * @param {Array} arr Array to be iterated over
+ * @param {Number} n Number of consecutive elements within a sub-array
+ * @param {Function} fn Function to operate on a sub-array (accepts the whole sub-array as input)
+ * @returns 
+ */
+const mapConsecutive = (arr, n, fn) =>
+    arr.slice(n - 1).map((v, i) => fn(arr.slice(i, i + n)));
+
+export default mapConsecutive;
