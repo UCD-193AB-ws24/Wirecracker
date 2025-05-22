0b839c80b88fbdf9eb2c4ff02caa1de40f7624c6
zhao-18 <zhao1455@gmail.com>
2025-05-22 13:21:21 -0700

    test: print what config vercel use from frontend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,6 +25,8 @@ const Login = () => {
         }
     };
 
+    console.log(process.env.NODE_ENV);
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-md">
